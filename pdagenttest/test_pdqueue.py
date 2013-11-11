1ac5e0ef553599b17dc65582ca1122208d575183
Div Shekhar <div@pagerduty.com>
2013-11-11 11:44:44 -0800

    Add PDQueue unit test class with dummy test

---
+++ pdagenttest/test_pdqueue.py
@@ -0,0 +1,14 @@
+
+import unittest
+
+
+class PDQueueTest(unittest.TestCase):
+
+    def test_dummy(self):
+        a = 1 + 1
+        self.assertEquals(2, a)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
