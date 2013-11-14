c927bbeb35e55c6a6cd42f144b3de56708a68d65
Div Shekhar <div@pagerduty.com>
2013-11-13 23:23:13 -0800

    Cleanup PDQueue & remove non-queue code

--- pdagent/pdqueue.py
+++ pdagent/pdqueue.py
@@ -39,46 +39,13 @@ class PDQueue(object):
             and os.access(self.queue_dir, os.W_OK)):
             raise Exception("Can't read/write to directory %s, please check permissions." % self.queue_dir)
 
-    def _readfile(self, fname):
-        fname_abs = os.path.join(self.queue_dir, fname)
-        f = open(fname_abs)
-        try:
-            return f.read()
-        finally:
-            f.close()
-
     # Get the list of queued files from the queue directory
     def _queued_files(self):
         fnames = [f for f in os.listdir(self.queue_dir) if f.startswith("pd_")]
         fnames.sort()
         return fnames
 
-    def _flush_queue(self):
-        file_names = self._queued_files()
-        # TODO handle related incidents e.g. if there is an ack for which a resolve is also present
-        for file_name in file_names:
-            file_path = ("%s/%s" % (self.queue_dir, file_name))
-            json_event_str = None
-            with open(file_path, "r") as event_file:
-                json_event_str = event_file.read()
-            incident_key, status_code = send_event_json_str(json_event_str)
-
-            # clean up the file only if we are successful, or if the failure was server-side.
-            if not (status_code >= 500 and status_code < 600): # success, or non-server-side problem
-                os.remove(file_path)
-
-    def flush(self):
-        with open("%s/lockfile" % self.queue_dir, "w") as lock_file:
-            try:
-                fcntl.lockf(lock_file.fileno(), fcntl.LOCK_EX | fcntl.LOCK_NB)
-                # We have acquired the lock here; let's flush the queue
-                self._flush_queue()
-            except IOError as e:
-                print "Error while trying to acquire lock on queue: %s" % str(e)
-            finally:
-                fcntl.lockf(lock_file.fileno(), fcntl.LOCK_UN)
-
-    def enqueue(self, event_json_str):
+    def enqueue(self, s):
         process_id = os.getpid()
         time_seconds = int(time.time())
         fname = "pd_%d_%d" % (time_seconds, process_id)
@@ -86,7 +53,7 @@ class PDQueue(object):
         if os.path.exists(fname_abs):
             raise AssertionError, "Queue entry file already exists: %s" % fname_abs
         with open(fname_abs, "w", 0600) as f:
-            f.write(event_json_str)
+            f.write(s)
         return fname
 
     def dequeue(self, consume_func):
@@ -103,11 +70,11 @@ class PDQueue(object):
             # TODO: handle missing file or other errors
             f = open(fname_abs)
             try:
-                json_event_str = f.read()
+                s = f.read()
             finally:
                 f.close()
             #
-            consumed = consume_func(json_event_str)
+            consumed = consume_func(s)
             #
             if consumed:
                 # TODO: handle/log delete error!
