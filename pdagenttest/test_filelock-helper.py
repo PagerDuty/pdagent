56fdb97877aa283a08bc8292f29aafd470ec29f9
Div Shekhar <div@pagerduty.com>
2013-11-14 15:20:41 -0800

    Add more filelock tests

--- pdagenttest/test_filelock-helper.py
+++ pdagenttest/test_filelock-helper.py
@@ -1,45 +1,55 @@
 
+import os
+import signal
 import sys
 import time
 
 from pdagent.filelock import FileLock, FileLockException
 from pdagenttest.test_filelock import TEST_LOCK_FILE
 
+lock = FileLock(TEST_LOCK_FILE, timeout=1)
+
 
 def test_spawn_ok():
     return 10
 
 def test_simple_lock():
-    l=FileLock(TEST_LOCK_FILE)
-    l.acquire()
+    lock.acquire()
     time.sleep(1)
-    l.release()
+    lock.release()
     return 20
 
 def test_lock_timeout():
-    l=FileLock(TEST_LOCK_FILE, timeout=1)
     try:
-        l.acquire()
+        lock.acquire()
     except FileLockException:
         return 30
     return 31
 
+def test_exit_without_release():
+    lock.acquire()
+    return 40
+
+def test_kill_releases_lock():
+    lock.acquire()
+    os.kill(os.getpid(), signal.SIGKILL)
+    return 50
 
 
 if __name__ == "__main__":
     args = sys.argv
-    if len(args) != 2: sys.exit(2) # wrong number of args
+    if len(args) != 2: sys.exit(2)  # wrong number of args
     #
     _test_func_name = args[1]
-    if not _test_func_name.startswith("test_"): sys.exit(5) # bad test name
+    if not _test_func_name.startswith("test_"): sys.exit(5)  # bad test name
     #
     main_module = sys.modules["__main__"]
     _test_func = getattr(main_module, _test_func_name, None)
-    if not _test_func: sys.exit(6) # no such test
+    if not _test_func: sys.exit(6)  # no such test
     #
     exit_code = _test_func()
-    if exit_code != 0 and exit_code < 10: sys.exit(7) # reserved exit code!
-    #
+    if exit_code is None: sys.exit(7)  # missing exit code
+    if exit_code != 0 and exit_code < 10: sys.exit(8)  # reserved exit code
     sys.exit(exit_code)
 
 
--- pdagenttest/test_filelock.py
+++ pdagenttest/test_filelock.py
@@ -13,11 +13,12 @@ TEST_HELPER_PY = os.path.join(os.path.dirname(os.path.abspath(__file__)), "test_
 TEST_LOCK_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), "test_filelock_lock.txt")
 
 
-def run_helper():
-    test_name = inspect.stack()[1][3]  # call the helper test with same name as the calling test
+def run_helper(test_name=None):
+    if not test_name:
+        test_name = inspect.stack()[1][3]  # default to the same name as the calling test method
     e = os.system("python %s %s" % (TEST_HELPER_PY, test_name))
-    e = e / 256  # needed on unix to get actual exit code
-    return e
+    exit_code, signal = e / 256, e % 256
+    return exit_code, signal
 
 
 class FileLockTest(unittest.TestCase):
@@ -25,21 +26,44 @@ class FileLockTest(unittest.TestCase):
     def setUp(self):
         if os.path.exists(TEST_LOCK_FILE):
             os.unlink(TEST_LOCK_FILE)
+        #
+        self.lock = FileLock(TEST_LOCK_FILE)
+        # Ensure that some other process is not holding the lock
+        self.lock.acquire()
+        self.lock.release()
 
     def test_spawn_ok(self):
         # this test just to check if we're spawning the helper correctly
         e = run_helper()
-        self.assertEqual(e, 10)
+        self.assertEqual(e, (10, 0))
+
+    def test_simple_lock_internal(self):
+        self.assertFalse(os.path.exists(TEST_LOCK_FILE))
+        self.lock.acquire()
+        self.assertTrue(os.path.exists(TEST_LOCK_FILE))
+        self.lock.release()
+        self.assertFalse(os.path.exists(TEST_LOCK_FILE))
 
     def test_simple_lock(self):
-        self.assertEqual(run_helper(), 20)
+        self.assertEqual(run_helper(), (20, 0))
+        self.assertFalse(os.path.exists(TEST_LOCK_FILE))
 
-    def test_lock_timeout(self):
-        l = FileLock(TEST_LOCK_FILE)
-        l.acquire()
-        e = run_helper()
-        self.assertEqual(e, 30)
-        l.release()
+    def test_simple_lock_file_exists(self):
+        open(TEST_LOCK_FILE, "w").write("-1\n")
+        self.assertEqual(run_helper("test_simple_lock"), (20, 0))
+        self.assertFalse(os.path.exists(TEST_LOCK_FILE))
+
+    def test_exit_without_release(self):
+        self.assertEqual(run_helper(), (40, 0))
+        self.lock.acquire()
+        self.lock.release()
+
+    def test_kill_releases_lock(self):
+        self.assertEqual(run_helper(), (0, 9))
+        self.assertTrue(os.path.exists(TEST_LOCK_FILE))
+        self.lock.acquire()
+        self.lock.release()
+        self.assertFalse(os.path.exists(TEST_LOCK_FILE))
 
 
 if __name__ == '__main__':
