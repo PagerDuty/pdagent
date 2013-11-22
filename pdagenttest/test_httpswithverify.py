7f0ea5636462855318f8e9650cb28098ba424d89
Anita Rao <anita@pagerduty.com>
2013-11-21 19:54:47 -0800

    Cert-validation test improvements.

--- pdagenttest/test_httpswithverify.py
+++ pdagenttest/test_httpswithverify.py
@@ -16,14 +16,11 @@ class PDVerifiedHttpsTest(unittest.TestCase):
             # if the server cert is valid, we connected to the server,
             # and server has complained to us about authorization.
             self.assertEqual(e.getcode(), 401)
-        except:
-            # we were not supposed to get other types of errors...
-            self.fail("Unexpected error %s" % str(sys.exc_info()[1]))
         else:
-            # ... and certainly not zero errors.
+            # we don't expect zero errors.
             self.fail("Didn't encounter expected error")
 
-    def test_invalid_cert(self):
+    def test_cert_with_different_domain(self):
         # we should get a CertificateError here because this url uses a
         # certificate issued to cloudfront.net
         self.assertRaises(
@@ -37,12 +34,9 @@ class PDVerifiedHttpsTest(unittest.TestCase):
                 # this is the right error.
                 pass
             else:
-                self.fail("Unexpected error %s" % str(sys.exc_info()[1]))
-        except:
-            # we were not supposed to get other types of errors...
-            self.fail("Unexpected error %s" % str(sys.exc_info()[1]))
+                raise
         else:
-            # ... and certainly not zero errors.
+            # we don't expect zero errors.
             self.fail("Didn't encounter expected error")
 
     def test_no_https(self):
@@ -53,11 +47,8 @@ class PDVerifiedHttpsTest(unittest.TestCase):
             # if the server cert is not checked, we will connect,
             # and will be denied access by the server.
             self.assertEqual(e.getcode(), 403)
-        except:
-            # we were not supposed to get other types of errors...
-            self.fail("Unexpected error %s" % str(sys.exc_info()[1]))
         else:
-            # ... and certainly not zero errors.
+            # we don't expect zero errors.
             self.fail("Didn't encounter expected error")
 
 if __name__ == '__main__':
