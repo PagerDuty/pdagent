627a04d1209053df194875100e922877e0855b79
Div Shekhar <div@pagerduty.com>
2013-12-03 14:08:24 -0800

    Update deb build script for backports, vagrant mount

--- deb.sh
+++ deb.sh
@@ -11,20 +11,12 @@
 #   rm pdagent_0.1_all.deb
 #   sh deb.sh
 #
-# Pushing the .deb into the vagrant VM:
-#
-#   # check the ssh proxy port - vm must be running
-#   vagrant ssh-config agent-lucid32
-#
-#   # scp into the vagrant vm - vm must be running
-#   # watch out for ssh key changes when VMs come & go
-#   # use correct localport instead of 2222
-#   scp -P 2222 pdagent_0.1_all.deb vagrant@localhost:~
-#
 # Testing the .deb:
 #
 #   vagrant ssh agent-lucid32
-#   sudo dpkg -i pdagent_0.1_all.deb
+#
+#   # project directory should be mounted in the VM at /vagrant
+#   sudo dpkg -i /vagrant/pdagent_0.1_all.deb
 #   which agent.py
 #   which pd-send.py
 #   python -c "import pdagent; print pdagent.__file__"
@@ -44,8 +36,9 @@ cp bin/*.py build-deb/usr/bin
 chmod +x build-deb/usr/bin/*.py
 
 echo pdagent...
-mkdir -p build-deb/usr/share/pyshared/pdagent
-cp pdagent/*.py build-deb/usr/share/pyshared/pdagent/
+mkdir -p build-deb/usr/share/pyshared
+find pdagent -type d -exec mkdir build-deb/usr/share/pyshared/{} \;
+find pdagent -type f -name "*.py" -exec cp {} build-deb/usr/share/pyshared/{} \;
 cp pdagent-0.1.egg-info build-deb/usr/share/pyshared/
 
 echo python-support...
@@ -54,7 +47,9 @@ cp python-pdagent.public build-deb/usr/share/python-support/
 find build-deb/usr/share/pyshared | grep "egg" | cut -c 10- >> build-deb/usr/share/python-support/python-pdagent.public
 find build-deb/usr/share/pyshared | grep "py$" | cut -c 10- >> build-deb/usr/share/python-support/python-pdagent.public
 
+echo ---- python-pdagent.public:
 cat build-deb/usr/share/python-support/python-pdagent.public
+echo ----
 
 
 fpm -s dir -t deb \
