3b08322b375a764486f4234cfd094136ad70f54e
Div Shekhar <div@pagerduty.com>
2013-12-04 17:32:13 -0800

    makedep clean up & work in progress

--- build-deb/makedeb.sh
+++ build-deb/makedeb.sh
@@ -34,19 +34,21 @@ echo bin...
 mkdir -p data/usr/bin
 cp ../bin/*.py data/usr/bin
 
+echo var...
+mkdir -p data/var/log/pdagent
+mkdir -p data/var/lib/pdagent/outqueue
+
 echo pdagent...
 mkdir -p data/usr/share/pyshared
 (cd .. && find pdagent -type d -exec mkdir build-deb/data/usr/share/pyshared/{} \;)
 (cd .. && find pdagent -type f -name "*.py" -exec cp {} build-deb/data/usr/share/pyshared/{} \;)
-#cp pdagent-0.1.egg-info data/usr/share/pyshared/
 
 echo python-support...
 mkdir -p data/usr/share/python-support
 echo pyversions=2.4- > data/usr/share/python-support/python-pdagent.public
 echo >> data/usr/share/python-support/python-pdagent.public
-find data/usr/share/pyshared -type f -name "*.py" | cut -c 5- >> data/usr/share/python-support/python-pdagent.public
-#find data/usr/share/pyshared | grep "egg" | cut -c 10- >> data/usr/share/python-support/python-pdagent.public
-#find data/usr/share/pyshared -name "*.py" | cut -c 10- >> data/usr/share/python-support/python-pdagent.public
+find data/usr/share/pyshared -type f -name "*.py" \
+    | cut -c 5- >> data/usr/share/python-support/python-pdagent.public
 
 echo ---- python-pdagent.public:
 cat data/usr/share/python-support/python-pdagent.public
@@ -54,25 +56,15 @@ echo ----
 
 
 fpm -s dir -t deb \
-    -n "pdagent" \
-    -v "0.1" \
-    -a all \
-    -d python \
-    -d python-support \
-    -C data \
+    --name "pdagent" \
+    --version "0.1" \
+    --architecture all \
+    --depends python \
+    --depends python-support \
     --post-install deb-postinst \
+    -C data \
     usr
 
 #    --prefix /usr \
-#    --prefix /opt/pagerduty-agent \
-
-#    $(SOURCEDIR)
-
-#    -C src \
 #    --deb-user pdagent
-
-
-#fpm -s dir -t deb -n $(NAME) -v $(VERSION) -d "nodejs (>= 0)" -a all \
-#         --prefix $(INSTALLDIR) --exclude .svn -C src $(SOURCEDIR)
-
 # --config-files /etc/redis/redis.conf -v 2.6.10 ./src/redis-server=/usr/bin redis.conf=/etc/redis
