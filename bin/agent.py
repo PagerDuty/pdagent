c1c296ccfa1e5d9dc238569d96570430595f1ce3
Div Shekhar <div@pagerduty.com>
2014-01-31 14:38:45 -0800

    Avoid double logging on agent normal exit

--- bin/agent.py
+++ bin/agent.py
@@ -187,11 +187,12 @@ class Agent(Daemon):
             except:
                 main_logger.error("Error stopping send thread", exc_info=True)
 
-            main_logger.info('*** pdagentd exiting!')
-            sys.exit(0)
         except SystemExit:
             main_logger.error('*** pdagentd exiting because of errors!')
             sys.exit(1)
+        else:
+            main_logger.info('*** pdagentd exiting normally!')
+            sys.exit(0)
 
 
 # read persisted, valid agent ID, or generate (and persist) one.
