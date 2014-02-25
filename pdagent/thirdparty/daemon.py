9b1c999f86603c164f789ecee07c4c473a5530a2
Div Shekhar <div@pagerduty.com>
2014-02-25 15:50:16 -0800

    Simplify daemon to be a function instead of a class

--- pdagent/thirdparty/daemon.py
+++ pdagent/thirdparty/daemon.py
@@ -1,4 +1,4 @@
-"Generic Unix daemon class"
+"Unix process daemonization"
 
 #
 # Author:    www.boxedice.com
@@ -27,108 +27,82 @@
 #        - Fixed unhandled exception if PID file is empty
 #
 
-# Core modules
 import atexit
 import os
 import sys
-import time
-
-from signal import SIGTERM
 
 
-class Daemon:
+def daemonize(
+        pidfile,
+        stdin=os.devnull, stdout=os.devnull, stderr=os.devnull
+        ):
     """
-    A generic daemon class.
+    Do the UNIX double-fork magic.
 
-    Usage: subclass the Daemon class and override the run() method
+    In the original process and the first child, this method will always throw
+    SystemExit (directly or via sys.exit) with an appropriate exit status.
+
+    On success, it will return normally in the daemonized grand-child process.
+
+    See Stevens' "Advanced Programming in the UNIX Environment"
+    for details (ISBN 0201563177)
+    http://www.erlenstar.demon.co.uk/unix/faq_2.html#SEC16
     """
-    def __init__(
-            self, pidfile,
-            stdin=os.devnull, stdout=os.devnull, stderr=os.devnull
-            ):
-        self.stdin = stdin
-        self.stdout = stdout
-        self.stderr = stderr
-        self.pidfile = pidfile
+    # Check for a pidfile to see if the daemon already runs
+    try:
+        pf = file(pidfile, 'r')
+        pid = int(pf.read().strip())
+        pf.close()
+    except (IOError, ValueError):
+        pid = None
 
-    def daemonize(self):
-        """
-        Do the UNIX double-fork magic, see Stevens' "Advanced
-        Programming in the UNIX Environment" for details (ISBN 0201563177)
-        http://www.erlenstar.demon.co.uk/unix/faq_2.html#SEC16
-        """
-        try:
-            pid = os.fork()
-            if pid > 0:
-                # Exit from First parent
-                _, status = os.waitpid(pid, 0)
-                if status:
-                    raise SystemExit("Error in second parent: %s" % status)
-                else:
-                    sys.exit(0)
-        except OSError, e:
-            raise SystemExit(
-                "fork #1 failed: %d (%s)\n" % (e.errno, e.strerror)
-                )
+    if pid:
+        message = "pidfile %s already exists. Is it already running?\n"
+        raise SystemExit(message % pidfile)
 
-        # Decouple from parent environment
-        os.chdir("/")
-        os.setsid()
-        os.umask(0)
-
-        # Do second fork
-        try:
-            pid = os.fork()
-            if pid > 0:
-                # Exit from second parent
+    # Do first fork
+    try:
+        pid = os.fork()
+        if pid > 0:
+            # Exit from First parent
+            _, status = os.waitpid(pid, 0)
+            if status:
+                raise SystemExit("Error in second parent: %s" % status)
+            else:
                 sys.exit(0)
-        except OSError, e:
-            raise SystemExit(
-                "fork #2 failed: %d (%s)\n" % (e.errno, e.strerror)
-                )
+    except OSError, e:
+        raise SystemExit("fork #1 failed: %d (%s)\n" % (e.errno, e.strerror))
 
-        if sys.platform != 'darwin':  # This block breaks on OS X
-            # Redirect standard file descriptors
-            sys.stdout.flush()
-            sys.stderr.flush()
-            si = file(self.stdin, 'r')
-            so = file(self.stdout, 'a+')
-            se = file(self.stderr, 'a+', 0)
-            os.dup2(si.fileno(), sys.stdin.fileno())
-            os.dup2(so.fileno(), sys.stdout.fileno())
-            os.dup2(se.fileno(), sys.stderr.fileno())
+    # Decouple from parent environment
+    os.chdir("/")
+    os.setsid()
+    os.umask(0)
 
-        # Write pidfile
-        atexit.register(self.delpid)  # Make sure pidfile is removed if we quit
-        pid = str(os.getpid())
-        file(self.pidfile, 'w+').write("%s\n" % pid)
+    # Do second fork
+    try:
+        pid = os.fork()
+        if pid > 0:
+            # Exit from second parent
+            sys.exit(0)
+    except OSError, e:
+        raise SystemExit("fork #2 failed: %d (%s)\n" % (e.errno, e.strerror))
 
-    def delpid(self):
-        os.remove(self.pidfile)
+    if sys.platform != 'darwin':  # This block breaks on OS X
+        # Redirect standard file descriptors
+        sys.stdout.flush()
+        sys.stderr.flush()
+        si = file(stdin, 'r')
+        so = file(stdout, 'a+')
+        se = file(stderr, 'a+', 0)
+        os.dup2(si.fileno(), sys.stdin.fileno())
+        os.dup2(so.fileno(), sys.stdout.fileno())
+        os.dup2(se.fileno(), sys.stderr.fileno())
 
-    def start(self):
-        """
-        Start the daemon
-        """
+    # Make sure pidfile is removed if we quit
+    def delpid():
+        os.remove(pidfile)
+    atexit.register(delpid)
 
-        # Check for a pidfile to see if the daemon already runs
-        try:
-            pf = file(self.pidfile, 'r')
-            pid = int(pf.read().strip())
-            pf.close()
-        except (IOError, ValueError):
-            pid = None
-
-        if pid:
-            message = "pidfile %s already exists. Is it already running?\n"
-            raise SystemExit(message % self.pidfile)
-
-        # Start the daemon
-        self.daemonize()
-        self.run()
-
-    def run(self):
-        """
-        You should override this method when you subclass Daemon. It will be
-        called after the process has been daemonized by start() or restart().
-        """
+    # Write pidfile
+    pid = str(os.getpid())
+    file(pidfile, 'w+').write("%s\n" % pid)
