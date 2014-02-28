2561815da17fcdb71d5eb5bf24f20e7b06a551e4
Anita Rao <anita@pagerduty.com>
2014-02-28 15:35:08 -0800

    Fix find command problem during simplification.

--- build-linux/make_package.sh
+++ build-linux/make_package.sh
@@ -91,7 +91,7 @@ echo = python modules...
 mkdir -p $_PY_SITE_PACKAGES
 cd ..
 find pdagent -type d -exec mkdir -p build-linux/$_PY_SITE_PACKAGES/{} \;
-find pdagent -type f -name "*.py" -o -name "ca_certs.pem" \
+find pdagent -type f \( -name "*.py" -o -name "ca_certs.pem" \) \
     -exec cp {} build-linux/$_PY_SITE_PACKAGES/{} \;
 cd -
 
