835390b9f84a3b5ac99a4e14de70d46a0b18a0b0
Div Shekhar <div@pagerduty.com>
2014-03-17 11:09:36 -0700

    Update tests for new outqueue permissions

--- pdagenttestinteg/test_20_process.sh
+++ pdagenttestinteg/test_20_process.sh
@@ -41,7 +41,8 @@ test "$SVC_KEY" != "CHANGEME" || {
 
 # stop agent and clear outqueue if required.
 test -z "$(agent_pid)" || stop_agent
-test $(ls $OUTQUEUE_DIR | wc -l) -eq 0 || sudo rm -r $OUTQUEUE_DIR/*
+sudo find $OUTQUEUE_DIR -type f -exec rm -f {} \;
+
 
 # modify agent check frequency so we wait for lesser time.
 sudo sed -i "s#^\#send_interval_secs.*#send_interval_secs=$SEND_INTERVAL_SECS#" $CONFIG_FILE
@@ -49,31 +50,36 @@ sudo sed -i "s#^\#send_interval_secs.*#send_interval_secs=$SEND_INTERVAL_SECS#" 
 # agent must flush out queue when it starts up.
 test_startup() {
   $BIN_PD_SEND -k $SVC_KEY -t acknowledge -i test$$_1 -f key=value -f foo=bar
+
+  test $(sudo find $OUTQUEUE_DIR -type f | wc -l) -eq 1
+  QUEUED_FILE=$(sudo find $OUTQUEUE_DIR -type f )
+  test $(sudo stat -c %a $QUEUED_FILE) = "640"
+
   $BIN_PD_SEND -k $SVC_KEY -t resolve -i test$$_1 -d "Testing"
 
-  test $(ls $OUTQUEUE_DIR/pdq_* | wc -l) -eq 2
+  test $(sudo find $OUTQUEUE_DIR -type f -name "pdq_*" | wc -l) -eq 2
 
   start_agent
   test -n "$(agent_pid)"
   sleep $(($SEND_INTERVAL_SECS / 2))  # enough time for agent to flush the queue.
-  test $(ls $OUTQUEUE_DIR | wc -l) -eq 2
-  test $(ls $OUTQUEUE_DIR/suc_* | wc -l) -eq 2
+  test $(sudo find $OUTQUEUE_DIR -type f | wc -l) -eq 2
+  test $(sudo find $OUTQUEUE_DIR -type f -name "suc_*" | wc -l) -eq 2
 }
 
 # agent must flush out queue when it wakes up.
 test_wakeup() {
-  test $(ls $OUTQUEUE_DIR/* | wc -l) -eq 2
+  test $(sudo find $OUTQUEUE_DIR -type f | wc -l) -eq 2
   $BIN_PD_SEND -k $SVC_KEY -t acknowledge -i test$$_2 -f baz=boo
   $BIN_PD_SEND -k $SVC_KEY -t resolve -i test$$_2 -d "Testing"
   # corrupt the latest enqueued file.
   echo "bad json" \
-    | sudo tee $(ls $OUTQUEUE_DIR/pdq_* | tail -n1) >/dev/null
+    | sudo tee $(sudo find $OUTQUEUE_DIR -type f -name "pdq_*" | tail -n1) >/dev/null
 
   sleep $(($SEND_INTERVAL_SECS * 3 / 2))  # sleep-time + extra-time for processing.
   # there must be one error file in outqueue; everything else must be cleared.
-  test $(ls $OUTQUEUE_DIR/* | wc -l) -eq 4
-  test $(ls $OUTQUEUE_DIR/err_* | wc -l) -eq 1
-  test $(ls $OUTQUEUE_DIR/suc_* | wc -l) -eq 3
+  test $(sudo find $OUTQUEUE_DIR -type f | wc -l) -eq 4
+  test $(sudo find $OUTQUEUE_DIR -type f -name "err_*" | wc -l) -eq 1
+  test $(sudo find $OUTQUEUE_DIR -type f -name "suc_*" | wc -l) -eq 3
 }
 
 test_startup
