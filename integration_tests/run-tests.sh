87c614ef497c77914b1f85f417e95b590805c099
Ashley Brooks <ashley.brooks.a@gmail.com>
2020-02-13 09:32:47 -0800

    Make script cleaner

--- integration_tests/run-tests.sh
+++ integration_tests/run-tests.sh
@@ -1,12 +1,11 @@
+#!/bin/bash
 set -e
 
 for test in $(dirname $0)/test_*.sh ; do 
   test_name=$(basename $test)
-  echo ; echo
-  echo "---------------->   Running $test_name <----------------" ; echo ; echo
+  echo -e "\n\n ---------------->   Running $test_name   <---------------- \n\n "
   set +x
   source $test
   set +x
-  echo ; echo
-  echo "---------------->   Succesfully Ran $test_name <----------------"
+  echo -e "\n\n---------------->   Succesfully Ran $test_name   <---------------- \n\n"
 done
